811deacc5fae662ee23e7c6ef44c32702a22d452
sharath <sharathc443@gmail.com>
2017-12-30 17:41:35 -0600

    Added employee table and Implemented Get actions

---
+++ EmployeeWEBAPIDEMO/Controllers/EmployeesController.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeDataAccess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace EmployeeWEBAPIDEMO.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+        public Employee Get(int id)
+        {
+            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            {
+                return entities.Employees.FirstOrDefault(e=>e.ID==id);
+            }
+        }
+    }
+}
